aba4abee85413cb3bbd186a1a808caf55d078b6f
Od1n-0 <pavel080201@gmail.com>
2026-06-07 01:09:30 +0300

    Homework 3

--- src/Homework_2/Homework_2.java
+++ src/Homework_2/Homework_2.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package Homework_2;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Homework_2 {
+    public static void main(String[] args) {
+        FourthTask();
+    }
+
+    public static void FirstTask() {
+        int b = 10;
+        int c = 4;
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("4*(b+c-1)/2 = " + (4 * (b + c - 1) / 2));
+    }
+
+    public static void SecondTask(){
+        int n = 26;
+        int a = n / 10;
+        int b = n % 10;
+        int c = a + b;
+        System.out.println("Исходное число: " + n);
+        System.out.println("Числа для сложения: " + a + " и " + b);
+        System.out.println("Сумма чисел: " + c);
+    }
+
+    public static void ThirdTask() {
+        int n = 126;
+        int a = n % 10;
+        int b = (n / 10) % 10;
+        int c = n / 100;
+        int d = a + b + c;
+        System.out.println("Исходное число: " + n);
+        System.out.println("Числа для сложения: " + a + ", " + b + " и " + c);
+        System.out.println("Сумма чисел: " + d);
+    }
+
+    public static void FourthTask(){
+        float n = 4.49F;
+        float x = n % 1;
+        System.out.println("Остаток при делении: " + x);
+        int a = (x < 0.5) ? (int) n : (int) n+ 1;
+        System.out.println("Число: " + n);
+        System.out.println("Округлено до: " + a);
+    }
+
+    public static void FifthTask(){
+        int q = 21;
+        int w = 8;
+        int a = q / w;
+        int b = q % w;
+        System.out.println(q + " / " + w + " = " + a + " и " + b + " в остатке");
+    }
+
+    public static void SixthTask(){
+        int a = 5;
+        int b = 3;
+        System.out.println("Переменная а заданная: " + a);
+        System.out.println("Переменная b заданная: " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("Переменная а полученная: " + a);
+        System.out.println("Переменная b полученная: " + b);
     }
 }
---
+++ src/Homework_3/Homework_3.java
@@ -0,0 +1,95 @@
+package Homework_3;
+
+import java.util.Scanner;
+
+public class Homework_3 {
+    public static void main(String[] args){
+        System.out.print("Enter task number(1-5): ");
+        String task = new Scanner(System.in).nextLine();
+        switch (task){
+            case "1":
+                FirstTask();
+                break;
+            case "2":
+                SecondTask();
+                break;
+            case "3":
+                ThirdTask();
+                break;
+            case "4":
+                FourthTask();
+                break;
+            case "5":
+                FifthTask();
+                break;
+        }
+    }
+    public static void FirstTask(){
+        System.out.print("Enter any number: ");
+        String number = new Scanner(System.in).nextLine();
+        try {
+            int num = Integer.parseInt(number);
+            if (num % 2 == 0){
+                System.out.println("You entered an even number");
+            }else {
+                System.out.println("You entered an odd number");
+            }
+        }catch (NumberFormatException e) {
+            System.out.println("Incorrect value");
+        }
+    }
+    public static void SecondTask(){
+        System.out.print("Enter the outside temperature: ");
+        String temperature = new Scanner(System.in).nextLine();
+        try {
+            int temp = Integer.parseInt(temperature);
+            if (temp > -5){
+                System.out.println("Warm");
+            } else if (temp <= -5 && temp > -20) {
+                System.out.println("Normal");
+            }else {
+                System.out.println("Cold");
+            }
+        }catch (NumberFormatException e) {
+            System.out.println("Incorrect value");
+        }
+    }
+    public static void ThirdTask(){
+        int num = 10;
+        System.out.print("While loop: ");
+        while (num <= 20){
+            System.out.print(num * num + " ");
+            num += 1;
+        }
+        System.out.print("\nFor loop: ");
+        for (int numfor = 10; numfor <= 20; numfor++){
+            System.out.print(numfor * numfor + " ");
+        }
+    }
+    public static void FourthTask(){
+        int seven = 0;
+        int sum = 7;
+        while (seven < 100){
+            seven = sum + seven;
+            System.out.print(seven + " ");
+        }
+    }
+    public static void FifthTask(){
+        System.out.print("Enter a positive number: ");
+        String number = new Scanner(System.in).nextLine();
+        try {
+            int sum = 0;
+            int num = Integer.parseInt(number);
+            if (num > 0){
+                for (int value = 0; value <= num; value++){
+                    sum += value;
+                }
+                System.out.println(sum);
+            }else {
+                System.out.println("Negative number");
+            }
+        }catch (NumberFormatException e) {
+            System.out.println("Incorrect value");
+        }
+    }
+}
