20d846a4aaa0b2553ac6126dd046a867620c1fd8
Od1n-0 <pavel080201@gmail.com>
2026-06-12 19:38:36 +0300

    Corrections HW2 & HW3

--- src/Homework_2/Homework_2.java
+++ src/Homework_2/Homework_2.java
@@ -2,28 +2,44 @@ package Homework_2;
 
 public class Homework_2 {
     public static void main(String[] args) {
-        FourthTask();
+        System.out.println("Task 1");
+        calculate();
+        System.out.println("-".repeat(50));
+        System.out.println("Task 2");
+        getSum();
+        System.out.println("-".repeat(50));
+        System.out.println("Task 3");
+        getSumNumbers();
+        System.out.println("-".repeat(50));
+        System.out.println("Task 4");
+        getRemainder();
+        System.out.println("-".repeat(50));
+        System.out.println("Task 5");
+        getDivision();
+        System.out.println("-".repeat(50));
+        System.out.println("Task *");
+        getReplacement();
     }
 
-    public static void FirstTask() {
+    public static void calculate() {
         int b = 10;
         int c = 4;
+        int a = 4 * (b + c - 1) / 2;
         System.out.println("b = " + b);
         System.out.println("c = " + c);
-        System.out.println("4*(b+c-1)/2 = " + (4 * (b + c - 1) / 2));
+        System.out.println("a = 4 * (b + c - 1) / 2 = " + a);
     }
 
-    public static void SecondTask(){
+    public static void getSum(){
         int n = 26;
         int a = n / 10;
         int b = n % 10;
-        int c = a + b;
         System.out.println("Исходное число: " + n);
         System.out.println("Числа для сложения: " + a + " и " + b);
-        System.out.println("Сумма чисел: " + c);
+        System.out.println("Сумма чисел: " + (a + b));
     }
 
-    public static void ThirdTask() {
+    public static void getSumNumbers() {
         int n = 126;
         int a = n % 10;
         int b = (n / 10) % 10;
@@ -34,8 +50,8 @@ public class Homework_2 {
         System.out.println("Сумма чисел: " + d);
     }
 
-    public static void FourthTask(){
-        float n = 4.49F;
+    public static void getRemainder(){
+        float n = 4.89F;
         float x = n % 1;
         System.out.println("Остаток при делении: " + x);
         int a = (x < 0.5) ? (int) n : (int) n+ 1;
@@ -43,15 +59,13 @@ public class Homework_2 {
         System.out.println("Округлено до: " + a);
     }
 
-    public static void FifthTask(){
+    public static void getDivision(){
         int q = 21;
         int w = 8;
-        int a = q / w;
-        int b = q % w;
-        System.out.println(q + " / " + w + " = " + a + " и " + b + " в остатке");
+        System.out.println(q + " / " + w + " = " + (q/w) + " и " + (q%w) + " в остатке");
     }
 
-    public static void SixthTask(){
+    public static void getReplacement(){
         int a = 5;
         int b = 3;
         System.out.println("Переменная а заданная: " + a);
--- src/Homework_3/Homework_3.java
+++ src/Homework_3/Homework_3.java
@@ -8,31 +8,18 @@ public class Homework_3 {
             System.out.print("Enter task number(1-5) or exit(6): ");
             String task = new Scanner(System.in).nextLine();
             switch (task){
-                case "1":
-                    FirstTask();
-                    break;
-                case "2":
-                    SecondTask();
-                    break;
-                case "3":
-                    ThirdTask();
-                    break;
-                case "4":
-                    FourthTask();
-                    break;
-                case "5":
-                    FifthTask();
-                    break;
-                case "6":
-                    System.exit(0);
-                default:
-                    System.out.println("Incorrect value");
-                    break;
+                case "1" -> getNumber();
+                case "2" -> getTemperature();
+                case "3" -> getSquares();
+                case "4" -> getSequence();
+                case "5" -> getSum();
+                case "6" -> System.exit(0);
+                default -> System.out.println("Incorrect value");
             }
         }
     }
 
-    public static void FirstTask(){
+    public static void getNumber(){
         System.out.print("Enter any number: ");
         String number = new Scanner(System.in).nextLine();
         try {
@@ -47,7 +34,7 @@ public class Homework_3 {
         }
     }
 
-    public static void SecondTask(){
+    public static void getTemperature(){
         System.out.print("Enter the outside temperature: ");
         String temperature = new Scanner(System.in).nextLine();
         try {
@@ -64,7 +51,7 @@ public class Homework_3 {
         }
     }
 
-    public static void ThirdTask(){
+    public static void getSquares(){
         int num = 10;
         System.out.print("While loop: ");
         while (num <= 20){
@@ -75,24 +62,28 @@ public class Homework_3 {
         for (int numfor = 10; numfor <= 20; numfor++){
             System.out.print(numfor * numfor + " ");
         }
+        System.out.println("\n");
     }
 
-    public static void FourthTask(){
+    public static void getSequence(){
         int sum = 7;
         while (sum < 100){
             System.out.print(sum + " ");
             sum += 7 ;
         }
+        System.out.println("\n");
     }
 
-    public static void FifthTask(){
+    public static void getSum(){
         System.out.print("Enter a positive number: ");
         String number = new Scanner(System.in).nextLine();
         try {
             int sum = 0;
             int num = Integer.parseInt(number);
-            if (num > 0){
-                for (int value = 0; value <= num; value++){
+            if (num == 1){
+                System.out.println(1);
+            }else if (num > 1){
+                for (int value = 0; value < num; value++){
                     sum += value;
                 }
                 System.out.println(sum);
